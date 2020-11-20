12d595f109715672610ca0231f07a3dd8ab894a1
diohate <diogolopesmartins@hotmail.com>
2020-11-20 16:36:30 -0300

    contiuação do exercicio

--- POO/class/exemplo-02.php
+++ POO/class/exemplo-02.php
@@ -37,6 +37,22 @@ class Carro {
         $this->ano = $ano;
     }
 
+    public function exibir(){
+
+        return array(
+            "modelo"=>$this->getModelo(),
+            "motor"=>$this->getMotor(),
+            "ano"=>$this->getAno()
+        );
+    }
+
 }
+
+$gol= new Carro();
+$gol->setModelo("Gol GT");
+$gol->setMotor("1.6");
+$gol->setAno("2017");
  
+print_r($gol->exibir());
+
 ?>
