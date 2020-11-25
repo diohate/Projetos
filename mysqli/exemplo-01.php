c67ae1ba8030de8a99ac1504fb9f68484f3801e3
diohate <diogolopesmartins@hotmail.com>
2020-11-25 17:02:19 -0300

    quarta_25_11_2020

---
+++ mysqli/exemplo-01.php
@@ -0,0 +1,29 @@
+<?php 
+
+ $conn = new mysqli("localhost", "root", "", "dbphp7");
+
+ if ($conn->connect_error) {
+
+    echo "Error: " . $conn->connect_error;
+ }
+
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, desssenha) VALUES(?, ?)");
+
+$stmt->bind_param("ss", $login, $pass);
+
+$login = "user";
+
+$pass = "12345";
+
+$stmt->execute();
+
+$login = "root";
+
+$pass = "!@#$";
+
+$stmt->execute();
+
+
+
+
+?>
