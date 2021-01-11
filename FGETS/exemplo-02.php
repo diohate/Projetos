9e67effc079c4e57654bdb4c837ded10e5ebd141
diohate <42249948+diohate@users.noreply.github.com>
2021-01-11 16:48:46 -0300

    fgets-1

---
+++ FGETS/exemplo-02.php
@@ -0,0 +1,13 @@
+<?php 
+
+$filename = "logo.png";
+
+$base64 = base64_encode(file_get_contents($filename));
+
+$fileinfo = new finfo(FILEINFO_MIME_TYPE);
+
+$mimetype = $fileinfo->file($filename);
+
+echo "data:" . $mimetype . ";base64," . $base64;
+
+?>
