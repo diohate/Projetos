38bbfb35685daf6ec121b0ef39b18a51f58d69b6
diohate <42249948+diohate@users.noreply.github.com>
2021-01-21 14:17:21 -0300

    composer e phpmailer

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cf03175c9cc08defd2fc16751f0cffda5491d4d1',
+    'name' => 'diohate/composer',
+  ),
+  'versions' => 
+  array (
+    'diohate/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cf03175c9cc08defd2fc16751f0cffda5491d4d1',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
