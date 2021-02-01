310124765d1448a1895578e4c9140844ffb1c97f
diohate <42249948+diohate@users.noreply.github.com>
2021-02-01 13:14:55 -0300

    01022021

---
+++ seguranca/exemplo-01.php
@@ -0,0 +1,22 @@
+<?php 
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+    $cmd = escapeshellcmd($_POST["cmd"]);
+
+    echo "<pre>";
+
+    $comando = system("dir C:", $retorno);
+
+    echo "</pre>";
+
+}
+
+?>
+
+<form method="post">
+
+    <input type="text" name="cmd">
+    <button type="submit">Enviar</button>
+
+</form>
